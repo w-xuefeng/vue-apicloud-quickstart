ed1c93ba74e982822df9198a7ee83bed089aac17
w-xuefeng <xuefeng@kcalb.wang>
2018-10-14 21:59:23 +0800

    fix：rollback

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -5,7 +5,6 @@ const glob = require('glob')
 const fs = require('fs')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -82,13 +81,6 @@ let configBase = {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, // replace ExtractTextPlugin.extract({..})
-          'css-loader'
-        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
