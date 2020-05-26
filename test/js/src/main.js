30aeb877df042bba31341500e2461e93aff93559
w-xuefeng <xuefeng@wangxuefeng.com.cn>
2020-05-26 18:05:30 +0800

    feat: add test-wifi-sync script

--- test/js/src/main.js
+++ test/js/src/main.js
@@ -6,5 +6,8 @@ import pages from '@/config/pages.json'
 Vue.config.productionTip = false
 
 Vue
-  .use(VAQ, { pages, debugOnPC: process.env.NODE_ENV !== 'production' })
+  .use(VAQ, {
+    pages,
+    // debugOnPC: process.env.NODE_ENV !== 'production'
+  })
   .init({ el: '#app', render: h => h(App) })
